5734a79a542efbc860ab84f2ab176331712592be
CFE <hello@teamcfe.com>
2017-02-02 09:38:21 -0800

    9 - Making Model Changes

--- src/videos/models.py
+++ src/videos/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
-    embed_code = models.TextField()
+    title       = models.CharField(max_length=120)
+    embed_code  = models.TextField()
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
     def __str__(self): # __unicode__
-        return self.embed_code
+        return self.title
 
 
 
