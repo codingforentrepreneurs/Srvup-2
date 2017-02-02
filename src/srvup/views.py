e8f115dd0fe5c46591912a6adca3f4dc24ddeb7b
CFE <hello@teamcfe.com>
2017-02-02 10:18:50 -0800

    14 - Context Basics

--- src/srvup/views.py
+++ src/srvup/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
@@ -13,13 +14,18 @@ def home(request):
 
 class HomeView(View):
     def get(self, request, *args, **kwargs): # GET -- retrieve view / list view / search
-        return HttpResponse("Hello")
+        
+        context = {
+            "name": "Justin",
+            "random_number": random.randint(500, 1000)
+        }
+        return render(request, "home.html", context)
 
-    def post(self, request, *args, **kwargs): # POST -- create view
-        return HttpResponse("Hello")
+    # def post(self, request, *args, **kwargs): # POST -- create view
+    #     return HttpResponse("Hello")
 
-    def put(self, request, *args, **kwargs): # PUT -- update view
-        return HttpResponse("Hello")
+    # def put(self, request, *args, **kwargs): # PUT -- update view
+    #     return HttpResponse("Hello")
 
-    def delete(self, request, *args, **kwargs): # DELETE -- delete view
-        return HttpResponse("Hello")
+    # def delete(self, request, *args, **kwargs): # DELETE -- delete view
+    #     return HttpResponse("Hello")
