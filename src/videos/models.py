35d753ab054c7e2aa6a378383a2eda6991f781b4
CFE <hello@teamcfe.com>
2017-02-02 16:54:58 -0800

    19 - Auto Generate Slugs with Signals

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,28 +1,28 @@
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
 # Create your models here.
 class Video(models.Model):
-    # id        = models.AutoField(primary_key=True) # 1, 2, 3, 4, 
     title       = models.CharField(max_length=120)
     slug        = models.SlugField(blank=True)
     embed_code  = models.TextField()
-    updated     = models.DateTimeField(auto_now=True) # last saved
-    timestamp   = models.DateTimeField(auto_now_add=True) # time added
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self): # __unicode__
+    def __str__(self): 
         return self.title
 
-    """
-    @property
-    def pk(self):
-        return self.id
-    """
+def pre_save_video_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(pre_save_video_receiver, sender=Video)
 
 
 
-'''
+# def post_save_video_receiver(sender, instance, created, *args, **kwargs):
+#     if not instance.slug:
+#         instance.slug = slugify(instance.title) #.save()
+#         instance.save()
 
-python manage.py makemigrations
-python manage.py migrate
-
-'''
+# post_save.connect(post_save_video_receiver, sender=Video)
