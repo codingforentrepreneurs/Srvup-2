f270011761027f4d885005a3bd8656bffac03f61
CFE <hello@teamcfe.com>
2017-02-03 12:26:44 -0800

    26 - Serving Static Files

--- src/srvup/settings.py
+++ src/srvup/settings.py
@@ -120,3 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static_in_env'),
+]
+
+VENV_PATH = os.path.dirname(BASE_DIR)
+# print(VENV_PATH)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root') # AWS S3
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+
+
+
+
+
--- src/srvup/urls.py
+++ src/srvup/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
+
+
 
 from .views import home, HomeView
 from videos.views import (
@@ -36,3 +40,14 @@ urlpatterns = [
     url(r'^videos/(?P<slug>[\w-]+)/delete/$', VideoDeleteView.as_view(), name='video-delete'),
 
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
