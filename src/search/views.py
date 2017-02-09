c6a42b0b97b26d7af5b968e07cf4a764965a2bda
CFE <hello@teamcfe.com>
2017-02-09 15:33:13 -0800

    64 - Basic Search Results

--- src/search/views.py
+++ src/search/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.views.generic import View
 
@@ -7,5 +8,12 @@ from courses.models import Course, Lecture
 
 class SearchView(View):
     def get(self, request, *args, **kwargs):
-        print(request.GET.get('q')) 
-        return render(request, "search/default.html", {})
+        query = request.GET.get('q')
+        qs = None
+        if query:
+            qs = Course.objects.all().lectures().filter(
+                    title__icontains=query,
+                    category__title__icontains=query,
+                    )
+            c_qs = Category.objects.filter(title__icontains=query)
+        return render(request, "search/default.html", {"qs": qs, "c_qs": c_qs})
