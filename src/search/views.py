cd8c7bcc58c0832d7a49b9bcee9108e9b3a3270f
CFE <hello@teamcfe.com>
2017-02-09 15:47:56 -0800

    65 - Complex Lookups with Q

--- src/search/views.py
+++ src/search/views.py
@@ -1,4 +1,4 @@
-
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import View
 
@@ -10,10 +10,29 @@ class SearchView(View):
     def get(self, request, *args, **kwargs):
         query = request.GET.get('q')
         qs = None
+        c_qs = None
+        l_qs = None
         if query:
+            lec_lookup =  Q(title__icontains=query)\
+             | Q(description__icontains=query)
+            # lookup2 =  Q(title__icontains=query) & Q(description__icontains=query)
+            query_lookup = lec_lookup | Q(category__title__icontains=query)\
+             | Q(category__description__icontains=query)\
+             | Q(lecture__title__icontains=query)\
+            
             qs = Course.objects.all().lectures().filter(
-                    title__icontains=query,
-                    category__title__icontains=query,
-                    )
-            c_qs = Category.objects.filter(title__icontains=query)
-        return render(request, "search/default.html", {"qs": qs, "c_qs": c_qs})
+                        query_lookup
+                    ).distinct()
+            
+            qs_ids = [x.id for x in qs]
+
+            cat_lookup = Q(primary_category__in=qs_ids) | Q(secondary_category__in=qs_ids)
+            c_qs = Category.objects.filter(lec_lookup | cat_lookup).distinct()
+            l_qs = Lecture.objects.filter(lec_lookup).distinct()
+
+        context = {"qs": qs, "c_qs": c_qs, "l_qs": l_qs}
+        return render(request, "search/default.html", context)
+
+
+
+
