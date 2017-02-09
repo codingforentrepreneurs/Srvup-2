aaf65138e36c8d3f87b5d8de337866c67b5ce299
CFE <hello@teamcfe.com>
2017-02-09 15:55:44 -0800

    66 - Search Template Tag

---
+++ src/search/templatetags/search_form.py
@@ -0,0 +1,7 @@
+from django import template
+register = template.Library()
+
+
+@register.inclusion_tag('search/snippets/search_form.html')
+def search_form(request):
+    return {"request": request}
