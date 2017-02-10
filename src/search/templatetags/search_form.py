3bf170bfe7e7a1ea4772452b26c9ecb4fd43f4eb
CFE <hello@teamcfe.com>
2017-02-09 16:55:56 -0800

    67 - Improve Navigation

--- src/search/templatetags/search_form.py
+++ src/search/templatetags/search_form.py
@@ -3,5 +3,5 @@ register = template.Library()
 
 
 @register.inclusion_tag('search/snippets/search_form.html')
-def search_form(request):
-    return {"request": request}
+def search_form(request, navbar=False):
+    return {"request": request, 'navbar':navbar}
